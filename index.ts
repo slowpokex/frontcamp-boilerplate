f9df72a3f2805471349dfb7816a42c8a95e0a879
Mikita Isakau <harley26.gold@gmail.com>
2018-11-03 13:46:56 +0300

    Initial commit

--- index.ts
+++ index.ts
@@ -8,6 +8,6 @@ const DIST_FOLDER: string = process.env.DIST_FOLDER || './dist';
 const HOST: string = process.env.HOST || 'http://localhost';
 const PORT: number = +process.env.PORT || 3000;
 
-app.get('/*', express.static(join(DIST_FOLDER)));
+app.use(express.static(join(DIST_FOLDER)));
 
 app.listen(PORT, () => console.log(`Listening at ${HOST}:${PORT}/`));
